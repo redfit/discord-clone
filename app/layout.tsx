407a9e96fd18a7a3462ab234cd06337e30f50cfd
redfit <daisuke.fuchiwaki@kaminashi.com>
2023-10-06 00:11:15 +0900

    Create Server Model

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
+
 import { ClerkProvider } from '@clerk/nextjs'
-import { ThemeProvider } from '@/components/providers/theme-provider'
+
 import { cn } from '@/lib/utils'
+
+import { ModalProvider } from '@/components/providers/modal-provider'
+import { ThemeProvider } from '@/components/providers/theme-provider'
+
+import './globals.css'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -27,6 +32,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
